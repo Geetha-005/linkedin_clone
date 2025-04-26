b2bc623c189da5f051a21feacccf2710c2a40213
Geetha-005 <chillageetha323@gmail.com>
2025-04-27 01:30:22 +0530

    edit profile backend api completed

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,7 +1,5 @@
 import multer from "multer"
 
-const upload=multer({storage})
-
 
 let storage=multer.diskStorage({
     destination:(req,file,cb)=>{
@@ -11,5 +9,6 @@ let storage=multer.diskStorage({
         cb(null,file.originalname)
     }
 })
+const upload=multer({storage})
 
 export default upload
